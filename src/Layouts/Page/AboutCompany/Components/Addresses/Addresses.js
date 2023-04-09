b9746e3c521d641d8dd22cb86ad41255c357f40a
Игорь <45472581+nostra78@users.noreply.github.com>
2023-04-09 15:21:06 +0000

    сделал 2 колонки, паддинги и маргины

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,15 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-4 mt-4 divid'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-2'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
-            </div>
+        <div className='col ms-5 d-inline-flex flex-column gap-4 divid justify-content-between'>
+                <div className='d-inline-flex flex-column gap-4'>
+                    <div className='title'>Адреса заведений</div>
+                    <div className='d-inline-flex flex-column gap-2'>
+                        <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                        <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                    </div>
+                </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
