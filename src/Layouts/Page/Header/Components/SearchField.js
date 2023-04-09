a9c1027ae9f489897c0ef023d5cca7b668e1d5c5
Игорь <45472581+nostra78@users.noreply.github.com>
2023-04-09 11:08:56 +0000

    Обновил стиль SerchField, Primary & Secondary b

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -8,22 +8,24 @@ const WrapperInput = styled.div`
 `;
 
 const Input = styled.input`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #152842;
+left: 8px !important;     
+outline: none;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #94A3B8;
     width: 100%;
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        background: #F8FAFC; opacity: 0.8;
     }
     &:focus {
-        background: white;
+        background: #F8FAFC;
+        color: #0F172A;
     }
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
+    
     position: absolute;
     box-sizing:border-box;
     right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
@@ -34,8 +36,8 @@ const SearchIconWrapper = styled.div`
 function SearchField() {
     return (
         <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
-            <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
+            <Input className='p-2id' placeholder='Найти на странице...'/>
+            <SearchIconWrapper className='p-2id'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
                 <IconOnlyButton icon={<SearchIcon/>}/>
             </SearchIconWrapper>
         </WrapperInput>
